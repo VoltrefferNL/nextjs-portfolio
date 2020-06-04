ea1794f0e1a98caa2aadaed94e4f4f5a93327e5a
Niels de Visser <nielsdevisser@niels-Destkop.local>
2020-06-04 16:13:33 +0100

    Added working links

--- pages/content/IntroText.js
+++ pages/content/IntroText.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
 
+const Twitter = "https://twitter.com/DeVoltreffer";
+const Github = "https://github.com/VoltrefferNL";
+const Linkedin = "https://www.linkedin.com/in/niels-de-visser-5b718412a/";
+
+
 const IntroText = () => {
   return (
     <div>
 
-<p>Hi, I'm <span className="bold">Niels</span>,<br></br> I am Fullstack Software Developer that has just entered the Tech space. Passionate about coding, learning new skills, travelling and riding my bike. Would you like to know more about me? Check out my projects on <span className="bold"><a href="www.github.com" >Github</a></span>, read some of my blog post on my <span className="bold"><a href="www.LinkedIn.com" >LinkedIn</a></span> or reach out on <span className="bold"><a href="www.Twitter.com" >Twitter</a></span>. 
+<p>Hi, I'm <span className="bold">Niels</span>,<br></br> I am Fullstack Software Developer that has just entered the Tech space. Passionate about coding, learning new skills, travelling and riding my bike. Would you like to know more about me? Check out my projects on <span className="bold"><a href={Github} >Github</a></span>, read some of my blog post on my <span className="bold"><a href={Linkedin} >LinkedIn</a></span> or reach out on <span className="bold"><a href={Twitter} >Twitter</a></span>. 
 <br></br><br></br>
 I am currently updating my portfolio, please comeback soon.
 </p>
