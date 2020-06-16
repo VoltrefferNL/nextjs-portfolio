9038a856d8c5b811a745e5be408af1407aa3bf37
Niels de Visser <nielsdevisser@niels-Destkop.local>
2020-06-16 17:11:21 +0100

    Added working build

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d50755097df7426b52fe";
+/******/ 	var hotCurrentHash = "016664a1a226da3a3326";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
