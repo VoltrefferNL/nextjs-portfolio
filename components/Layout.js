82fbb487362445eee9ce47562201a75eed7b46e2
Niels de Visser <nielsdevisser@niels-Destkop.local>
2020-06-19 12:44:10 +0100

    Added Meta Tags

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,20 @@ import styles from "./layout.module.css";
 const Layout = (props) => (
   <div className={styles.container}>
     <Head>
-      <title>Niels de Visser | Portfolio</title>
+      <title>Niels de Visser | Software Developer</title>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Niels de Visser | Software Developer" />
+      <meta property="og:site_name" content="Niels de Visser | Software Developer" />
+      <meta property="og:description" content="A personal portfolio, where I share my projects, write about things I learn, and share additional thoughts." />
+      <meta property="og:url" content="https://www.devisser.tech/" />
+      <meta name="twitter:card" content="summary"></meta>
+      <meta name="twitter:title" content="Niels de Visser | Software Developer"></meta>
+      <meta name="twitter:site" content="@VoltrefferNL"></meta>
+      <meta name="twitter:creator" content="@VoltrefferNL"></meta>
+      <meta name="keywords" content="Portfolio, CSS, JavaScript, Blog, Contact" />
+      <meta name="author" content="Niels de Visser" />
       <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous"></link>
       <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet"></link>
